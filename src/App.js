3df728065f61ffb85c1c44369a9924904f97c41a
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2023-07-26 09:06:43 -0300

    Ajustes e documentação código.

--- src/App.js
+++ src/App.js
@@ -8,18 +8,20 @@ import { useState , useEffect } from 'react';
  * @returns 
  */
 function Layout(){
+  
+  // Renderiza o componente
   return (
     <>
       <h1>Menu principal</h1>
       <nav>      
         <ol>
           <li>
-            <Link to="/inicio/-1">
+            <Link to="/frmcadastrocliente/-1">
               Incluir
             </Link>
-          </li>
+          </li>         
           <li>
-            <Link to="/listar">
+            <Link to="/listarcliente">
               Listar(Alterar, Excluir)
             </Link>
           </li>          
@@ -37,6 +39,8 @@ function Layout(){
  * @returns 
  */
 function NoPage() {
+  
+  // Renderiza o componente
   return (
       <div>
         <h2>404 - Página não encontrada</h2>
@@ -45,57 +49,48 @@ function NoPage() {
 };
 
 /**
- * Página inicial.
+ * Componente formulário que insere ou altera cliente.
  * 
  * @returns 
  */
-function Inicio(){
+function FrmCadastroCliente(){
 
-  // Recupera o parâmetro
+  // Recupera o parâmetro do componente
   const { alterarId } = useParams();
 
-  // Estados inciais das variáveis do formulário   
-  const [clienteId, setClienteId] = useState(0)  
+  // Estados inciais das variáveis do componente   
+  const [clienteId, setClienteId] = useState(0);
   const [nome, setNome] = useState('');
   const [cpf, setCpf] = useState('');
-
   const [resultado, setResultado] = useState('');  
 
   // Renderiza a lista de clientes.
   useEffect(() => {
     
     const getCliente = async () => {
-      const resp = await fetch(`http://localhost:8000/cliente/${alterarId}`);
-      const data = await resp.json();
-      //Atualiza os dados se existir
-      if (data.clienteId > 0) {
+      //Se foi passado um parametro
+      if (alterarId > 0) {      
+        //Consulta o cliente
+        const response = await fetch(`http://localhost:8000/cliente/${alterarId}`);
+        const data = await response.json();
+        //Atualiza os dados
         setClienteId(data.clienteId);
         setNome(data.nome);
         setCpf(data.cpf);
       }      
     };
 
-    //Se tem algum cliente para alterar, busca os dados do cliente.
-    if (alterarId > 0) {
-      getCliente(); 
-    }
-
-  }, [alterarId])
-
-  //Verifica se é para inserir ou alterar.
-  const handleSubmit = (event) => {
-      if (alterarId > 0) {
-        handleSubmitUpdate(event);
-      } else {
-        handleSubmitInsert(event);
-      }
-  }
+    //Se tem algum cliente para alterar, busca os dados do cliente.    
+    getCliente(); 
+  }, [alterarId]);
 
   // Submissão do formulário para inserir.
   const handleSubmitInsert = (event) => {
+
     // Impede o recarregamento da página
     event.preventDefault();   
     
+    //Dados do formulário a serem enviados
     const dados =  { 
           'clienteId': clienteId,
           'nome': nome,
@@ -107,15 +102,16 @@ function Inicio(){
         method : 'post',
         headers : {'Content-Type': 'application/json'},
         body: JSON.stringify(dados)}) //Converte os dados para JSON
-       .then((res) => res.json()) //Converte a resposta para JSON
+       .then((response) => response.json()) //Converte a resposta para JSON
        .then((data) => setResultado(data.message)); // Atribui a resposta ao resultado
   
     // Limpa os campos do formulário.
     limpar();
-  }
+  };
 
   // Submissão do formulário atualizar.
   const handleSubmitUpdate = (event) => {
+
     // Impede o recarregamento da página
     event.preventDefault();   
     
@@ -123,32 +119,32 @@ function Inicio(){
           'clienteId': clienteId,
           'nome': nome,
           'cpf': cpf
-    }
+    };
 
     //Endereço da API + campos em JSON
     fetch(`http://localhost:8000/cliente/${clienteId}`, {
         method : 'put',
         headers : {'Content-Type': 'application/json'},
         body: JSON.stringify(dados)}) //Converte os dados para JSON
-       .then((res) => res.json()) //Converte a resposta para JSON
+       .then((response) => response.json()) //Converte a resposta para JSON
        .then((data) => setResultado(data.message)); // Atribui a resposta ao resultado
   
     // Limpa os campos do formulário.
     limpar();
-  }
+  };
 
   // Limpa os campos do formulário.     
   const limpar = () => { 
     setClienteId(0);
     setNome('');
     setCpf('');
-  }
+  };
 
-  // Renderiza o formulário
+  // Renderiza o componente formulário
   return (
-      <form name="FrmCadastroCliente" method="post" onSubmit={handleSubmit}>          
-          {alterarId < 0 ? (<label><h2>1 - Formulário Cadastro Cliente</h2></label>):
-            (<label><h2>1 - Formulário Alteração Cliente</h2></label>)}
+    <>      
+      <form name="FrmCadastroCliente" method="post" onSubmit={alterarId < 0 ? handleSubmitInsert: handleSubmitUpdate}>
+          <label><h2> {(alterarId < 0) ? (<div>1 - Formulário Cadastro Cliente</div>) : (<div>1 - Formulário Alteração Cliente</div>)} </h2></label>
           <label>ClienteId: 
           <input type="text" size="10" name="clienteId" value={clienteId} onChange={(event) => setClienteId(event.target.value)}/></label><br/>
           <label>Nome: 
@@ -159,6 +155,42 @@ function Inicio(){
           <input type="submit" name="Cadastrar" value="Cadastrar"/><br/><br/>
           <label>Resultado: {resultado} </label>
       </form>
+      </>
+  );
+};
+
+/**
+ * Componente de exclusão de cliente.
+ * 
+ * @returns 
+ */
+function FrmExcluirCliente() {
+
+  // Recupera o parâmetro do componente
+  const { clienteId } = useParams();
+
+  // Estados inciais das variáveis do componente
+  const [resultado, setResultado] = useState('');
+  
+  // Renderiza a lista de clientes.
+  useEffect(() => {
+
+    // Exclui um cliente
+    const excluirCliente = async () => {
+      //Endereço da API + campos em JSON
+      fetch(`http://localhost:8000/cliente/${clienteId}`, {method : 'delete'}) 
+      .then((response) => response.json()) //Converte a resposta para JSON
+      .then((data) => setResultado(data.message)); // Atribui a resposta ao resultado
+    };
+
+    excluirCliente();
+  }, [clienteId]);
+
+  // Renderiza o componente
+  return (
+    <div>      
+       <label>Resultado: {resultado} </label>
+    </div>
   );
 }
 
@@ -167,59 +199,48 @@ function Inicio(){
  * 
  * @returns 
  */
-function Listar(){
-
-  // Inicializa as variáveis
+function ListarCliente(){
+  
+  // Estados inciais das variáveis do componente
   const navigate = useNavigate();
   const [clientes, setClientes] = useState([])
-  const [resultado, setResultado] = useState('');
-
-  // Busca os clientes cadastrados no servidor.
-  const getClientes = () => {
-    fetch("http://localhost:8000/cliente")
-      .then(response => {return response.json()})
-      .then(data => {setClientes(data)})
-  }
-
+  
   // Renderiza a lista de clientes.
   useEffect(() => {
-    getClientes()
-  }, [])
 
-  // Exclui um cliente
-  const excluirCliente = (clienteId) => {
-     //Endereço da API + campos em JSON
-     fetch(`http://localhost:8000/cliente/${clienteId}`, {
-      method : 'delete'}) 
-     .then((res) => res.json()) //Converte a resposta para JSON
-     .then((data) => setResultado(data.message)); // Atribui a resposta ao resultado
+    // Busca os clientes cadastrados no servidor.
+    const getClientes = () => {
+      fetch("http://localhost:8000/cliente")
+        .then(response => {return response.json()}) //Converte a resposta para JSON
+        .then(data => {setClientes(data)}) // Atribui a resposta ao cliente
+    };
 
-     window.location.reload(false);
-  }
-  
+    getClientes();
+  }, []);
+
+  // Renderiza o componente
   return (
     <div>
-        <h2>2 - Listar(Alterar, Excluir)</h2>
-        
-        {clientes.length > 0 ? (
-        <table border='1'> 
-          <td>Id</td> <td>Nome</td><td>CPF</td><td>Editar</td><td>Excluir</td>          
-          {clientes.map(cliente => (
-            <tr>
+        <h2>2 - Listar(Editar, Excluir)</h2>        
+        <div>
+          <table border='1'> 
+            <td>Id</td> <td>Nome</td> <td>CPF</td> <td>Editar</td> <td>Excluir</td>          
+            {clientes.map(cliente => (
+              <tr>
                 <td> {cliente.clienteId} </td>
                 <td> {cliente.nome}</td>
                 <td> {cliente.cpf}</td>
                 <td> 
-                    <button onClick={()=> {navigate(`/inicio/${cliente.clienteId}`)}}>Alterar</button>
+                  <button onClick={() => {navigate(`/frmcadastrocliente/${cliente.clienteId}`)}}>Editar</button>
                 </td>                
-                <td>
-                    <button onClick={() => {excluirCliente(cliente.clienteId)}}>Excluir</button>
+                <td>                  
+                  <button onClick={() => {navigate(`/frmexcluircliente/${cliente.clienteId}`)}}>Excluir</button>
                 </td>
-            </tr>
-          ))}
-        </table>        
-        ):<label>Tabela vazia!</label>}      
-        <br/><label>Resultado: {resultado} </label>
+              </tr>
+            ))}
+          </table>
+          <br/>          
+        </div>
      </div>
   );
 }
@@ -233,11 +254,12 @@ function MenuPrincipal() {
     return (      
       <BrowserRouter>
         <Routes>
-            <Route path='/' element={<Layout />}>
-                <Route path='inicio/:alterarId' element={<Inicio />} />
-                <Route path='listar' element={<Listar />} />                
-                <Route path='*' element={<NoPage />} />
-            </Route>
+          <Route path='/' element={<Layout />}>
+            <Route path='frmcadastroCliente/:alterarId' element={<FrmCadastroCliente />} />
+            <Route path='frmexcluircliente/:clienteId' element={<FrmExcluirCliente />} />
+            <Route path='listarcliente' element={<ListarCliente />} />                
+            <Route path='*' element={<NoPage />} />
+          </Route>
         </Routes>        
       </BrowserRouter>    
     );
